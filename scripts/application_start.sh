e104fdd115fefb6312e1393a2b43fee4d49f9395
Kevin-Aaaquil <eshansingh2409@gmail.com>
2022-02-06 20:20:57 +0530

    🐛

--- scripts/application_start.sh
+++ scripts/application_start.sh
@@ -4,4 +4,4 @@ cd /home/ubuntu/dev/deploy-test
 yarn install
 yarn build
 # pm2 start npm --name "server" -- start
-pm2 start build/index.js --name "deploy-test"
+pm2 restart build/index.js --name "deploy-test"
